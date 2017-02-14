b3c42e83ade31a2a4514cc5e2a4361cbbb16463d
John Talio <johntalio@yahoo.com>
2017-02-14 16:34:37 -0500

    Only routes for new posts and a post for creating posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
 end
