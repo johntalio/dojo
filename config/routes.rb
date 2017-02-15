e21dafc30467a3d8dc83581b6af8c56a6e8d1fff
John Talio <johntalio@yahoo.com>
2017-02-15 09:08:07 -0500

    Add routes for show for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   get '/posts/new' => 'posts#new'
-  post '/posts' => 'posts#create'
+  post '/posts' => 'posts#create', as: 'posts'
+  get '/posts/:id' => 'posts#show', as: 'post'
 
   get '/users/new' => 'users#new'
   post '/users' => 'users#create', as: 'users'
+  get '/users/:id' => 'users#show', as: 'user'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create', as: 'logins'
