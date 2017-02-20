260d23bb2fe2c8924b9c949c9ecdeaeb4363f46d
John Talio <johntalio@yahoo.com>
2017-02-20 18:07:28 -0500

    Create paths for new and create paths plus the game_params private method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,26 @@
 class GamesController < ApplicationController
-  def index
+  def show
+    @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    if logged_in?
+      @game = current_user.games.new(game_params)
+      if @game.save
+        redirect_to @game
+      else
+        render 'new'
+      end
+    end
+  end
+
+  private
+
+    def game_params
+      params.require(:game).permit(:name, :platform, :esrb_rating, :genre, :score)
+    end
 end
