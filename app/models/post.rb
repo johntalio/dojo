2eb39c40a13d2e20b2d6d8e5baac92da1d08a921
John Talio <johntalio@yahoo.com>
2017-02-21 18:07:59 -0500

    Allow users to write more in posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
 
   validates :title, :body, presence: true
   validates :title, length: {minimum: 3}
-  validates :body, length: {in: 5..100}
+  validates :body, length: {in: 5..300}
 end
