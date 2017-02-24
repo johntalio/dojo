9f6a222c1d0f99c3abac39916a7801568cc54c6f
John Talio <johntalio@yahoo.com>
2017-02-24 13:00:52 -0500

    Need a controller to post comments through reacT?

---
+++ app/controllers/game_comments_controller.rb
@@ -0,0 +1,10 @@
+# class GameCommentsController < ApplicationController
+#   def create
+#     if logged_in?
+#       @game_comment = current_user.games.game_comments.new()
+#       if @game_comment.save
+#         redirect_to @game
+#       end
+#     end
+#   end
+# end
