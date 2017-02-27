24a87c9edabfa1836ef2cf12e54d7ad753c07882
John Talio <johntalio@yahoo.com>
2017-02-27 13:55:15 -0500

    Working on postComment function

--- app/assets/javascripts/components/comments/ShowPage.es6.jsx
+++ app/assets/javascripts/components/comments/ShowPage.es6.jsx
@@ -1,16 +1,19 @@
 class ShowPage extends React.Component {
   constructor() {
     super()
-    this.state = {showForm: false}
+    this.state = {showForm: false, comments: []}
     this.handleClick = this.handleClick.bind(this)
+    this.postComment = this.postComment.bind(this)
   }
 
-  // postComment(comment) {
-  //   $.ajax({
-  //     method: 'POST'
-  //     url: '/game_comments'
-  //   })
-  // }
+  postComment(comment) {
+    $.ajax({
+      method: 'POST',
+      url: '/games/:game_id/game_comments'
+    }).done(response => {
+      this.setState({comments: response})
+    })
+  }
 
   handleClick() {
     this.setState({showForm: true})
@@ -20,7 +23,7 @@ class ShowPage extends React.Component {
     return(
       <div>
         <input id="comment-button" type="button" value="Leave a comment" onClick={this.handleClick} />
-        {this.state.showForm ? <CommentForm /> : null}
+        {this.state.showForm ? <CommentForm postComment={this.postComment} /> : null}
       </div>
     )
   }
